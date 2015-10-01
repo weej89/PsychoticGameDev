555a74222484d46ccfe71cf7c0137ace7c06f069
weej89 <weej89@gmail.com>
2015-10-01 01:13:31 -0500

    Working Flashlight
    Updated flashlight and battery settings.  Added comments and regions
    for formatting

---
+++ Psychotic/Assets/PsychoticGame/Scripts/FlashlightScript.cs
@@ -0,0 +1,50 @@
+﻿#region Using
+using UnityEngine;
+using System.Collections;
+#endregion
+
+public class FlashlightScript : MonoBehaviour
+{
+
+	#region Public Fields
+	public bool FlashlightOn;
+	#endregion
+
+	#region Private Fields
+	private float FlashlightBattery;
+	private Light flashLight;
+	#endregion
+
+	#region Start
+	/// <summary>
+	/// Initializes public and private fields
+	/// </summary>
+	void Start ()
+	{
+		FlashlightOn = false;
+		FlashlightBattery = 100f;
+		flashLight = this.GetComponent<Light>();
+	}
+	#endregion
+
+	#region Update
+	/// <summary>
+	/// Update this instance.
+	/// Tells the Game Component whether or not
+	/// the Light in the flashlight should be
+	/// turned on and if the battery is charged
+	/// </summary>
+	void Update ()
+	{
+		if (FlashlightOn && FlashlightBattery > 0) 
+		{
+			flashLight.intensity = 1;
+			FlashlightBattery -= .1f;
+		} 
+		else 
+		{
+			flashLight.intensity = 0;
+		}
+	}
+	#endregion
+}
---
+++ Psychotic/Assets/PsychoticGame/Scripts/FlashlightToggle.cs
@@ -0,0 +1,32 @@
+﻿#region Using
+using UnityEngine;
+using System.Collections;
+#endregion
+
+public class FlashlightToggle : MonoBehaviour {
+
+	private FlashlightScript flashLightScript;
+
+	#region Start
+	/// <summary>
+	/// Initializes private and public fields
+	/// </summary>
+	void Start () 
+	{
+		flashLightScript = GetComponent<FlashlightScript> ();
+	}
+	#endregion
+
+	#region Update
+	/// <summary>
+	/// Updates the flashlight object when the
+	/// registered key is pressed
+	/// </summary>
+	void Update () {
+		if (Input.GetKeyDown ("f")) 
+		{
+			flashLightScript.FlashlightOn=!flashLightScript.FlashlightOn;
+		}
+	}
+	#endregion
+}
