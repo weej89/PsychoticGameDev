724b25c70949838562a37ee4d7f83ab89dfbc804
waffles9992 <waffles9992@gmail.com>
2015-10-03 21:10:50 -0500

    Undergoing Controller Changes
    Game is not playable right now, in middle of changing player controller
    to a physics based one

---
+++ Psychotic/Assets/DeleteDemoText.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//When the player (or any designated trigger) walks through this object, it will delete
+//the demo text. Other wise, it could be viewed from any where.
+public class DeleteDemoText : MonoBehaviour {
+	public GameObject TextToDelete;
+	void Start () {
+		TextToDelete = GameObject.Find("DemoText");
+	}
+	void OnTriggerEnter(Collider DelText1) {
+		DestroyObject (TextToDelete);
+	}
+}
