cfc3532c7e7ccd26137e55606532f76c23d3e6af
weej89 <weej89@gmail.com>
2015-10-14 17:47:16 -0500

    Added VR Player Controller and Messed with Settings
    Also started on target area for player.  Settings will need to be
    adjusted from this point to accommodate VR.  Currently only getting
    about 40-50 normal fps

---
+++ Psychotic/Assets/PsychoticGame/Scripts/TargetArea.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetArea {
+
+	Vector3 center;
+	float radius;
+	double area;
+
+	public TargetArea(Vector3 _center, float _radius)
+	{
+		center = _center;
+		radius = _radius;
+
+		this.area = Mathf.PI * Mathf.Pow(radius, 2);
+	}
+
+	public Vector3 GetPointInTargetArea()
+	{
+		float x, z;
+		float xRange = Mathf.Abs(center.x - radius);
+		float zRange = Mathf.Abs(center.z - radius);
+		Vector3 point;
+
+		x = Random.Range(center.x - xRange, center.x + xRange);
+		z = Random.Range(center.z - zRange, center.z + zRange);
+
+		point = new Vector3(x, 0, z);
+		return point;
+	}
+}
