ece828c8746d35259f5f2eae8e368a9a6d212050
weej89 <weej89@gmail.com>
2015-11-12 04:28:08 -0600

    Opening Door Script

---
+++ Psychotic/Assets/PsychoticGame/Scripts/TerrainPainter.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class TerrainPainter : MonoBehaviour {
+
+	SplatPrototype splat = new SplatPrototype();
+	public Texture2D texture;
+
+	// Use this for initialization
+	void Start () {
+		//splat = (Texture2D)Resources.Load(texture);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
