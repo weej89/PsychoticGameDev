3f4f1e1ab8ee56cc45c4a1edd439f894024ede10
weej89 <weej89@gmail.com>
2015-11-22 05:39:26 -0600

    New Sounds and Such
    New zombies added with different placement.  Bathroom added.  Sound
    effects added to beginning of game.

--- Psychotic/Assets/PsychoticGame/Scripts/Triggers/EnemyAliveScript.cs
+++ Psychotic/Assets/PsychoticGame/Scripts/Triggers/EnemyAliveScript.cs
@@ -3,18 +3,30 @@ using System.Collections;
 
 public class EnemyAliveScript : MonoBehaviour 
 {
-	GameObject enemy;
+	GameObject[] enemy;
+	GameObject audioObject;
+	AudioSource sound;
 
 	void Awake()
 	{
-		enemy = GameObject.FindGameObjectWithTag("Enemy");
-		enemy.SetActive(false);
+		enemy = GameObject.FindGameObjectsWithTag("Enemy");
+		audioObject = GameObject.FindGameObjectWithTag("Zombie_Echo");
+		sound = audioObject.GetComponent<AudioSource>();
+
+		foreach(GameObject obj in enemy)
+			obj.SetActive(false);
 	}
 
 	void OnTriggerEnter(Collider other)
 	{
 		if(other.CompareTag("Player"))
-			enemy.SetActive(true);
+		{
+			foreach(GameObject obj in enemy)
+				obj.SetActive(true);
+
+			sound.Play();
+			this.gameObject.SetActive(false);
+		}
 	}
 
 
