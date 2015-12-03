363ca5019d51b85efb6b98239bf5b90381598b85
weej89 <weej89@gmail.com>
2015-12-03 06:36:53 -0600

    More Level Changes

--- Psychotic/Assets/PsychoticGame/Scripts/A_star/FringeSearchPath.cs
+++ Psychotic/Assets/PsychoticGame/Scripts/A_star/FringeSearchPath.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 
 public class FringeSearchPath : GridPath
 {
+
 	#region Constructor
 	/// <summary>
 	/// Initializes a new instance of the <see cref="FringeSearchPath"/> class.
---
+++ Psychotic/Assets/PsychoticGame/Scripts/Triggers/Trigger.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour 
+{
+	public string[] TriggerTags; 
+	public AudioSource Sound = null;
+	public string TagRef = null;
+	public int NumActivations = 0;
+	public bool StopOnExit = false;
+	public bool LoopSound = false;
+
+	public enum Trigger_Type{ON_ENTER, ON_STAY, ON_AWAKE}
+	public Trigger_Type TriggerType;
+	
+	protected GameObject[] enemy;
+	protected int activationCount = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		InitGameObjects();
+
+		if(TriggerType != Trigger_Type.ON_AWAKE)
+		{
+			foreach(GameObject obj in enemy)
+				obj.SetActive(false);
+		}
+		else
+		{
+			foreach(GameObject obj in enemy)
+				obj.SetActive(true);
+
+			Sound.Play();
+		}
+	}
+	
+	public void OnTriggerEnter(Collider other)
+	{
+		if(TriggerType == Trigger_Type.ON_ENTER)
+		{
+			foreach(string tag in TriggerTags)
+			{
+				if(other.CompareTag(tag))
+				{
+					SetTriggerActive(true, true);
+				}
+			}
+		}
+	}
+
+	public void OnTriggerStay(Collider other)
+	{
+		if(TriggerType == Trigger_Type.ON_STAY)
+		{
+			foreach(string tag in TriggerTags)
+			{
+				if(other.CompareTag(tag))
+				{
+					SetTriggerActive(true, true);
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(StopOnExit)
+		{
+			foreach(string tag in TriggerTags)
+			{
+				if(other.CompareTag(tag))
+				{
+					SetTriggerActive(false, false);
+				}
+			}
+		}
+	}
+
+	private void InitGameObjects()
+	{
+		if(TagRef != null)
+		{
+			enemy = GameObject.FindGameObjectsWithTag(TagRef);
+		}
+
+		Sound.loop = LoopSound;
+	}
+
+	private void SetTriggerActive(bool playSound, bool setActiveObjects)
+	{
+		foreach(GameObject obj in enemy)
+			obj.SetActive(setActiveObjects);
+
+		if(Sound != null)
+		{
+			if(playSound && !Sound.isPlaying)
+				Sound.Play();
+			else
+				Sound.Stop();
+		}
+
+		activationCount++;
+
+		if(NumActivations > 0 && activationCount >= NumActivations)
+		{
+			this.gameObject.SetActive(false);
+		}
+	}
+	
+}
